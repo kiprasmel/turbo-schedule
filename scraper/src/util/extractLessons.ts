4487a47925f7d4499efbad2e7e55cfce1e32ae02
Kipras Melnikovas <kipras@kipras.org>
2019-09-23 23:07:28 +0300

    Create `extractLessons` util
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ scraper/src/util/extractLessons.ts
@@ -0,0 +1,118 @@
+const removeNewline = (content: string) => {
+	return content.replace("\n", "");
+};
+
+const createLessonId = (dayIndex: number, timeIndex: number, lessonName: string | null): string => {
+	return `day:${dayIndex}/time:${timeIndex}/name:${lessonName}`;
+};
+
+export const extractLesson = (scheduleItem: CheerioElement, dayIndex: number, timeIndex: number) => {
+	/** the schedule item was useless and was deleted, so skip it */
+	if (!scheduleItem) {
+		return null;
+	}
+
+	// const idBase: string = `day-${dayIndex}/time-${timeIndex}`;
+
+	// console.log("scheduleItem", scheduleItem.children[0].children);
+
+	// const lessonItem = scheduleItem.children[0].children[0]; //.children[0];
+	// const lessonItem = scheduleItem.children[0].children[0].children[0].data;
+
+	const itemWithClassNameTeacherAndCabinet = scheduleItem.children[0] /** always skip this */.children;
+
+	/** */
+
+	const unsafeLessonName = itemWithClassNameTeacherAndCabinet[0].children;
+	/** skip `[1]` */
+	const unsafeTeacherName = itemWithClassNameTeacherAndCabinet[2];
+	/** skip `[3]` */
+	const unsafeCabinetName = itemWithClassNameTeacherAndCabinet[4];
+
+	if (!unsafeLessonName || !unsafeTeacherName || !unsafeCabinetName) {
+		let lesson = {
+			isEmpty: true,
+			dayIndex: dayIndex,
+			timeIndex: timeIndex,
+			id: "",
+			name: null,
+			teacher: null,
+			cabinet: null,
+		};
+
+		lesson.id = createLessonId(lesson.dayIndex, lesson.timeIndex, lesson.name);
+
+		// console.log("\nlesson", lesson);
+
+		return lesson;
+	}
+
+	/** */
+
+	const lessonName = unsafeLessonName[0].data || "";
+
+	const teacherName = unsafeTeacherName.data || "";
+
+	const cabinetName = unsafeCabinetName.data || "";
+
+	/** */
+
+	let lesson = {
+		isEmpty: false,
+		dayIndex: dayIndex,
+		timeIndex: timeIndex,
+		id: "",
+		name: removeNewline(lessonName),
+		teacher: removeNewline(teacherName),
+		cabinet: removeNewline(cabinetName),
+	};
+
+	lesson.id = createLessonId(lesson.dayIndex, lesson.timeIndex, lesson.name);
+
+	// Object.keys(lesson).map((key) => (lesson[key] = removeNewline(lesson[key])));
+
+	// console.log("\nlesson", lesson);
+
+	return lesson;
+};
+
+export const extractLessonsArray = (scheduleItemsArray: Array<CheerioElement>) => {
+	// scheduleItemsArray = scheduleItemsArray.splice(0, 15);
+	// const lessonsArray: Array<any> = scheduleItemsArray.map((scheduleItem, index) => extractLesson(scheduleItem, index));
+
+	// const lessonTimesArray: Array<string> = [
+	// 	"08:00",
+	// 	"08:55",
+	// 	"9:55",
+	// 	"10:55",
+	// 	"12:10",
+	// 	"13:10",
+	// 	"14:10",
+	// 	"15:05",
+	// 	"16:00",
+	// ];
+
+	// const workDaysArray: Array<string> = [
+	// 	"Pirmadienis",
+	// ]
+
+	// const workDaysArray: Array<number> = [0, 1, 2, 3, 4];
+
+	const howManyWorkdays: number = 5;
+	const howManyLessonsMax: number = 9; /** todo automatic */
+
+	const extractedLessonsArray: Array<any> = [];
+
+	/** go NOT from left to right & down, but from TOP to bottom & left */
+	for (let workDay = 0; workDay < howManyWorkdays; ++workDay) {
+		for (let lessonTime = 0; lessonTime < howManyLessonsMax; ++lessonTime) {
+			const lessonIndex = workDay + lessonTime * howManyWorkdays;
+
+			const extractedLesson = extractLesson(scheduleItemsArray[lessonIndex], workDay, lessonTime);
+
+			extractedLessonsArray.push(extractedLesson);
+		}
+	}
+
+	return extractedLessonsArray;
+};
