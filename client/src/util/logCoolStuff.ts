d3ee3e9db69ae98ae27fbd6e450f00616389de7d
Kipras Melnikovas <dev@kipras.org>
2019-09-24 01:37:58 +0300

    Create `logCoolStuff` util
    Signed-off-by: Kipras Melnikovas <dev@kipras.org>

---
+++ client/src/util/logCoolStuff.ts
@@ -0,0 +1,16 @@
+export const logCoolStuff = () => {
+	const message = `\
+
+Welcome!
+
+There's a public API available if you'd like to tinker:
+/api/**
+
+The github repository will soon be made available too!
+
+@author		Kipras Melnikovas (https://kipras.org) <kipras@kipras.org>
+
+`;
+
+	console.log(message);
+};
