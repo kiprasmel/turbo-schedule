79831cf184cfff76019bc06c001c19cfaf9cf5d3
Kipras Melnikovas <kipras@kipras.org>
2019-09-24 01:47:48 +0300

    Create `en` (English) translation dictionary
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ client/src/i18n/en.ts
@@ -0,0 +1,35 @@
+import { Dictionary } from "./Dictionary";
+
+const weekdays: Array<string> = [
+	"Monday", //
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+	"Sunday",
+];
+
+export const en: Dictionary = {
+	Language: "Language",
+	en: "English",
+	lt: "Lithuanian",
+	weekday: (index: number) => weekdays[index],
+	"I am": "I am",
+	"Student not found": (name: string) => `Student "${name}" not found...`,
+	"Go back and search for a different one": "Go back search for a different one",
+	"Loading data": "Loading data",
+	"Interested in what's coming next?": "Interested in what's coming next?",
+	"Enter email get notified": "Enter your email and get a notification once we (I, Kipras :D) do something awesome!",
+	"we promise to not spam!": "we promise to not spam!",
+	"you can cancel anytime": "you can cancel anytime",
+	"eg.": "eg.",
+	"Notify me about the updates!": "Notify me about the updates!",
+	"Email received successfully - thank You for trusting us:)":
+		"Email received successfully - thank You for trusting us:)",
+	Enter: "Enter",
+	Week: "Week",
+	"Go back": "Go back",
+	Lesson: "Lesson",
+	custom: (count: number, count2: number | undefined) => `Sup nerd ${count}, how're you doing, ${count2}`,
+};
