3045902630f7de335123aa0080a1531d6134ee0d
Kipras Melnikovas <kipras@kipras.org>
2019-09-24 01:48:01 +0300

    Create `lt` (Lithuanian) translation dicitonary
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ client/src/i18n/lt.ts
@@ -0,0 +1,35 @@
+import { Dictionary } from "./Dictionary";
+
+const weekdays: Array<string> = [
+	"Pirmadienis", //
+	"Antradienis",
+	"Trečiadienis",
+	"Ketvirtadienis",
+	"Penktadienis",
+	"Šeštadienis",
+	"Sekmadienis",
+];
+
+export const lt: Dictionary = {
+	Language: "Kalba",
+	en: "Anglų",
+	lt: "Lietuvių",
+	weekday: (index: number) => weekdays[index],
+	"I am": "Aš esu",
+	"Student not found": (name: string) => `Moksleivis "${name}" nerastas...`,
+	"Go back and search for a different one": "Grįžk ir ieškok kito!",
+	"Loading data": "Siurbiame duomenis...",
+	"Interested in what's coming next?": "Įdomu, kas bus toliau?",
+	"Enter email get notified": "Įvesk savo paštą ir gauk pranešimą, kai mes (aš, Kipras :D) padarom ką nors awesome!",
+	"we promise to not spam!": "prižadame ne'spam'inti!",
+	"you can cancel anytime": "galite bet kada atšaukti",
+	"eg.": "pvz.",
+	"Notify me about the updates!": "Pranešti man apie atnaujinimus!",
+	"Email received successfully - thank You for trusting us:)":
+		"El. paštas gautas sėkmingai - dėkojame už pasitikėjimą:)",
+	Enter: "Enter",
+	Week: "Savaitė",
+	"Go back": "Grįžti",
+	Lesson: "Pamoka",
+	custom: (count) => `county count ${count}`,
+};
