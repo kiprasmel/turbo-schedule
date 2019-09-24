d11a33152aec12949f90ae3c1e6e69a5b0d5c408
Kipras Melnikovas <kipras@kipras.org>
2019-09-25 00:32:02 +0300

    Create `AbsoluteCenter` component
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ client/src/common/absoluteCenter/AbsoluteCenter.tsx
@@ -0,0 +1,33 @@
+/**
+ * https://css-tricks.com/centering-css-complete-guide/
+ */
+
+import React from "react";
+
+// export interface IAbsoluteCenterProps {
+// 	style
+// }
+
+/**
+ * Issues with blurry borders
+ */
+
+// const AbsoluteCenter = ({ children }: React.Props<any>) => {
+// 	return (
+// 		<div style={{ height: "100vh", position: "relative" }}>
+// 			<div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+// 				{children}
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+const AbsoluteCenter = ({ children }: React.Props<any>) => {
+	return (
+		<div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+			<div style={{ overflow: "auto", resize: "both" }}>{children}</div>
+		</div>
+	);
+};
+
+export default AbsoluteCenter;
