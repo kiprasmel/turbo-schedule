c6106a1987011efcb4b98b336f569b373d27154f
Kipras Melnikovas <kipras@kipras.org>
2019-09-25 00:32:26 +0300

    Create misc `common` components
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ client/src/common/todo--AutoCompleteInput.tsx
@@ -0,0 +1,33 @@
+// import React, { ChangeEvent, useState } from "react";
+
+const AutoCompleteInput = () => null; /** TODO */
+// const AutoCompleteInput = () => {
+// 	const cooldownTimeMs: number = 300;
+
+// 	let lastKeyPressTime: Date;
+// 	let lastSearchTime: Date;
+
+// 	const [searchString, setSearchString] = useState("");
+
+// 	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+// 		const { value } = event.target;
+
+// 		setSearchString(value);
+
+// 		const currentTime: Date = new Date();
+
+// 		if (lastKeyPressTime.getTime() < currentTime.getTime() - cooldownTimeMs) {
+// 			// search
+// 		}
+
+// 		lastKeyPressTime = new Date();
+// 	};
+
+// 	return (
+// 		<div>
+// 			<input type="text" onChange={(e) => handleChange(e)} />
+// 		</div>
+// 	);
+// };
+
+export default AutoCompleteInput;
