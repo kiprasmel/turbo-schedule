2fd3ad5748969cc22ab514780185f5bab211dbe1
Kipras Melnikovas <kipras@kipras.org>
2019-09-25 04:24:04 +0300

    Update `StudentListModal` component & rm unneeded dependencies
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- client/src/components/studentSchedule/StudentListModal.tsx
+++ client/src/components/studentSchedule/StudentListModal.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import ReactModal from "react-modal";
+import { useTranslation } from "i18n/useTranslation";
 
 /**
  * http://reactcommunity.org/react-modal/accessibility/#app-element
@@ -10,7 +11,13 @@ interface ICloseBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	handleClose: () => any;
 	text?: string;
 }
-const CloseBtn: FC<ICloseBtnProps> = ({ handleClose, text = "Uždaryti", ...rest }) => {
+const CloseBtn: FC<ICloseBtnProps> = ({ handleClose, text, ...rest }) => {
+	const t = useTranslation();
+
+	if (!text) {
+		text = t("Close");
+	}
+
 	return (
 		<button
 			onClick={(_e) => handleClose()}
@@ -26,6 +33,8 @@ const CloseBtn: FC<ICloseBtnProps> = ({ handleClose, text = "Uždaryti", ...rest
 };
 
 const StudentListModal = ({ handleClose, lesson, isOpen, ...rest }: any) => {
+	const t = useTranslation();
+
 	return (
 		<>
 			<ReactModal
@@ -40,12 +49,14 @@ const StudentListModal = ({ handleClose, lesson, isOpen, ...rest }: any) => {
 				<CloseBtn handleClose={handleClose} style={{ marginTop: "1em" }} autoFocus />
 
 				<div style={{ marginTop: "1em" }}>
-					<p style={{ margin: 0 }}>Pamoka:</p>
-					{lesson.name || "Laisva"}
+					<p style={{ margin: 0 }}>{t("Lesson")}:</p>
+					{lesson.name || t("Empty__lesson")}
 				</div>
 
 				<div style={{ marginTop: "1em" }}>
-					<p>Mokiniai ({lesson.students.length}):</p>
+					<p>
+						{t("Students")} ({lesson.students.length}):
+					</p>
 					<ul style={{ listStyle: "disc", paddingLeft: "30px" }}>
 						{lesson.students.map((student: string, index: number) => (
 							<li key={index}>{student}</li>
