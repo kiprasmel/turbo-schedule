dbdfc266488523da499559a8f62a8599e1ad08e3
Kipras Melnikovas <kipras@kipras.org>
2019-10-12 16:48:35 +0300

    chore(client): Clean up `BackBtn`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- client/src/common/BackBtn.tsx
+++ client/src/common/BackBtn.tsx
@@ -5,12 +5,13 @@ import { useTranslation } from "../i18n/useTranslation";
 
 const BackBtn = () => {
 	const t = useTranslation();
+
 	return (
-		<div>
+		<>
 			<button className="btn btn--big" style={{ marginTop: "1em" }} onClick={(_e) => history.push("/")}>
 				{t("Go back")}
 			</button>
-		</div>
+		</>
 	);
 };
 
