59a5673e94bc07d95e26f4978423feceee8203bf
Kipras Melnikovas <kipras@kipras.org>
2019-10-19 01:55:35 +0300

    feat: Create `jest.config.js` & a yarn script for jest
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ server/test/jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+/**
+ * https://jestjs.io/docs/en/configuration
+ */
+
+const { defaults } = require('jest-config');
+
+module.exports = {
+	...defaults,
+	transform: {
+		"^.+\\.tsx?$": "ts-jest"
+	},
+	moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+}
