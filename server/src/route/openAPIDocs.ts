a58500476a2c1687cce5b87abb675c22ca52b1d8
Kipras Melnikovas <kipras@kipras.org>
2019-10-19 02:09:23 +0300

    fix(server): Resolve to correct path when serving docs
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/src/route/openAPIDocs.ts
+++ server/src/route/openAPIDocs.ts
@@ -1,8 +1,11 @@
 import fs from "fs-extra";
+import { join } from "path";
 import { RequestHandler } from "express";
 
 /** read initially */
-const _openAPIDocs: string = fs.readFileSync("./openAPI.lean.json", { encoding: "utf-8" });
+const _openAPIDocs: string = fs.readFileSync(join(__dirname, "../", "../", "./openAPI.lean.json"), {
+	encoding: "utf-8",
+});
 const openAPIDocs: string = JSON.parse(_openAPIDocs);
 
 export const openAPIDocsHandler: RequestHandler = (_req, res) => {
