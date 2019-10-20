b8e3d477e8c7415ef749eb3fd4c57c0eace2e60d
Kipras Melnikovas <kipras@kipras.org>
2019-10-20 21:25:38 +0300

    fix(server): Handle exceptions when reading/parsing docs
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/src/route/openAPIDocs.ts
+++ server/src/route/openAPIDocs.ts
@@ -3,10 +3,25 @@ import { join } from "path";
 import { RequestHandler } from "express";
 
 /** read initially */
-const _openAPIDocs: string = fs.readFileSync(join(__dirname, "../", "../", "./openAPI.lean.json"), {
-	encoding: "utf-8",
-});
-const openAPIDocs: string = JSON.parse(_openAPIDocs);
+
+const leanDocsPath = join(__dirname, "../", "../", "./openAPI.lean.json");
+
+let _openAPIDocs: string = "{}";
+let openAPIDocs: string = "{}";
+
+try {
+	if (!fs.pathExistsSync(leanDocsPath)) {
+		_openAPIDocs = "{}";
+	} else {
+		_openAPIDocs = fs.readFileSync(leanDocsPath, {
+			encoding: "utf-8",
+		});
+	}
+
+	openAPIDocs = JSON.parse(_openAPIDocs);
+} catch (err) {
+	console.error("! openAPIDocs: Failed reading / parsing docs!");
+}
 
 export const openAPIDocsHandler: RequestHandler = (_req, res) => {
 	res.setHeader("Content-Type", "application/json");
