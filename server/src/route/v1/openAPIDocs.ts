02001cf5758fcd19946f4a90befaa69b881de016
Kipras Melnikovas <kipras@kipras.org>
2019-10-23 00:50:03 +0300

    feat(server): Improve the sh1t outta `openAPIDocs`!
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/src/route/v1/openAPIDocs.ts
+++ server/src/route/v1/openAPIDocs.ts
@@ -2,31 +2,49 @@ import fs from "fs-extra";
 import { join } from "path";
 import { RequestHandler } from "express";
 
-/** read initially */
+import { isProd } from "../../../src/util/isProd";
 
-const leanDocsPath = join(__dirname, "../", "../", "./openAPI.lean.json");
+/** read initially (@note - wont update until you restart!) */
 
-let _openAPIDocs: string = "{}";
-let openAPIDocs: string = "{}";
+const leanDocsPath = join(__dirname, "..", "..", "..", "generated", "./openAPI.lean.json");
 
-try {
-	if (!fs.pathExistsSync(leanDocsPath)) {
-		_openAPIDocs = "{}";
-	} else {
-		_openAPIDocs = fs.readFileSync(leanDocsPath, {
-			encoding: "utf-8",
-		});
+let openAPIDocsJSON: string = "{}";
+
+export const openAPIDocsJSONHandler: RequestHandler = (_req, res, next) => {
+	try {
+		if (!fs.pathExistsSync(leanDocsPath)) {
+			openAPIDocsJSON = "{}";
+			console.log("  ! openAPIDocsJSON not found!");
+		} else {
+			openAPIDocsJSON = fs.readJsonSync(leanDocsPath, {
+				encoding: "utf-8",
+			});
+		}
+
+		res.setHeader("Content-Type", "application/json");
+		res.send(openAPIDocsJSON);
+		return !isProd() ? next() : res.end();
+	} catch (err) {
+		console.error("! openAPIDocsJSON: Failed reading / parsing docs!", err);
+		return !isProd() ? next(err) : res.end();
 	}
-
-	openAPIDocs = JSON.parse(_openAPIDocs);
-} catch (err) {
-	console.error("! openAPIDocs: Failed reading / parsing docs!");
-}
-
-export const openAPIDocsHandler: RequestHandler = (_req, res) => {
-	res.setHeader("Content-Type", "application/json");
-	res.send(openAPIDocs);
 };
 
-console.log("openAPIDocs");
-console.log(openAPIDocs);
+export const openAPIDocsHTMLHandler: RequestHandler = (_req, res, next) => {
+	try {
+		const html: string = `
+		<redoc spec-url="http://localhost:5000/api/v1/docs.json"></redoc>
+
+		<!--  NOTE - the script MUST come AFTER the 'redoc' thing lmao  -->
+		<script src="https://cdn.jsdelivr.net/npm/redoc/bundles/redoc.standalone.js"> </script>
+	`;
+
+		res.setHeader("Content-Type", "text/html");
+		res.send(html);
+
+		return !isProd() ? next() : res.end();
+	} catch (err) {
+		console.error(err);
+		return next(err); /** this is intended to be handled by `next` either way */
+	}
+};
