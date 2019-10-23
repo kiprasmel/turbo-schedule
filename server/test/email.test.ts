bc56341fadc746ff9be5c842cc52eafb86bdbb04
Kipras Melnikovas <kipras@kipras.org>
2019-10-23 20:31:06 +0300

    feat(tests): Update `email` route tests
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/test/email.test.ts
+++ server/test/email.test.ts
@@ -3,20 +3,35 @@ import { app } from "../src/server";
 
 describe("/email API", () => {
 	it("should accept a new email & return it back", async () => {
+		const email: string = new Date().getTime() + ".test@kipras.org";
+
 		const res: request.Response = await request(app)
 			.post(`/api/v1/email`)
-			.send({
-				email: "test@kipras.org",
-			});
+			.send({ email });
 
 		expect(res.status).toBe(200);
 
 		const looselyExpectedObj = {
-			emailEntry: {
-				email: "test@kipras.org",
-			},
+			emailEntry: { email },
 		};
 
 		expect(res.body).toMatchObject(looselyExpectedObj);
+	});
+
+	it("should not accept a duplicate email & should return the old one", async () => {
+		const email: string = new Date().getTime() + ".test@kipras.org";
+
+		const makeRequest = async (): Promise<request.Response> =>
+			await request(app)
+				.post(`/api/v1/email`)
+				.send({ email });
+
+		const res1: request.Response = await makeRequest();
+		const res2: request.Response = await makeRequest();
+
+		expect(res1.status).toBe(200);
+
+		expect(res2.status).toBe(403);
+		expect(res2.body).toMatchObject({ emailEntry: email, error: "Email already exists" });
 	});
 });
