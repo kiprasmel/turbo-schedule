1843188e7c38d312aa834d86049a753a3f843edc
Kipras Melnikovas <kipras@kipras.org>
2019-11-13 22:09:05 +0200

    chore(common): Simplify exports @ `index.ts`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,14 +1,5 @@
-import { getYYYYMMDD } from "./util/getYYYYMMDD";
-import { delay } from "./util/delay";
+export { getYYYYMMDD } from "./util/getYYYYMMDD";
+export { delay } from "./util/delay";
 
-import { IStudent, Student } from "./model/Student";
+export { IStudent, Student } from "./model/Student";
 export { IEmail, Email } from "./model/Email";
-
-export {
-	/** utils */
-	getYYYYMMDD, //
-	delay,
-	/** models */
-	IStudent,
-	Student,
-};
