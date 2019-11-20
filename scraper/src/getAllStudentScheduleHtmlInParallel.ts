0ad5093572310c1ce27777ae43ac97ae70a75d10
Kipras Melnikovas <kipras@kipras.org>
2019-11-20 22:58:34 +0200

    feat(scraper): Retry on failed scrapes! ⛑
    This ensures that we always successfully scrape 100% of the profiles.
    
    Previously, there might've been too many network requests to the website
    we're scraping from, and we'd get a 503 error of "service unavailable".
    
    It would result in some students NOT having their profiles available
    in our app.
    This is exactly the scenario we're handling - we retry later if a
    request failed.
    
    Let's goooo!
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- scraper/src/getAllStudentScheduleHtmlInParallel.ts
+++ scraper/src/getAllStudentScheduleHtmlInParallel.ts
@@ -1,6 +1,14 @@
 import { IStudent } from "./model/Student";
 import { scrapeStudentSchedule } from "./util/scrapeStudentSchedule";
 
+interface IPromiseObj<T = any> {
+	studentData: IStudent;
+	startedPromise: Promise<T>;
+	succeeded: boolean;
+}
+
+const startPromise = async (studentData: IStudent) => await scrapeStudentSchedule(studentData);
+
 /**
  * TODO - this name is not describing this function correctly :D
  *
@@ -9,31 +17,82 @@ import { scrapeStudentSchedule } from "./util/scrapeStudentSchedule";
  *
  */
 export const getAllStudentScheduleHtmlInParallel = async (studentsDataArray: Array<IStudent>): Promise<Array<any>> => {
-	let scrapableSchedulePromiseArray: Array<Promise<any>> = studentsDataArray.map((studentData) => {
-		return scrapeStudentSchedule(studentData);
-	});
+	let scrapableSchedulePromiseArray: Array<IPromiseObj<Array<any> | undefined>> = studentsDataArray.map(
+		(studentData) => {
+			return {
+				studentData,
+				startedPromise: startPromise(studentData),
+				succeeded: false,
+			};
+		}
+	);
 
 	// console.log("scheduelsToScrape", scrapableSchedulePromiseArray.splice(0, 1));
 
-	/**
-	 * TODO
-	 *
-	 * Figure out the optimal way of scraping
-	 * without blocking the website & getting a firewall block :D
-	 *
-	 * Note:
-	 * `Promise.all` is used to wait for all promises to finish
-	 * and only then return the array.
-	 */
-	let allStudentSchedules: Array<any> = await Promise.all(
-		scrapableSchedulePromiseArray.map(async (scrapableSchedulePromise) => {
-			/** scrape one-by-one */
+	let allStudentSchedules: Array<any> = [];
+	let howManyFailedThisLoop: number = 0;
 
-			const scrapedSchedule = await scrapableSchedulePromise;
+	const maxRetryCount: number = 5; /** probably config-worthy */
+	let howManyTimesWeRetried: number = 0;
 
-			return scrapedSchedule;
-		})
-	);
+	do {
+		// if (howManyFailedThisLoop > 0) {
+		const msg: string = `\nNew loop cycle (${howManyTimesWeRetried}) - howManyFailed(Previous)Loop = ${howManyFailedThisLoop};\n`;
+		console.log(msg);
+		// }
+
+		/** reset */
+		howManyFailedThisLoop = 0;
+
+		/**
+		 * TODO
+		 *
+		 * Figure out the optimal way of scraping
+		 * without blocking the website & getting a firewall block :D
+		 *
+		 * Note:
+		 * `Promise.all` is used to wait for all promises to finish
+		 * and only then return the array.
+		 */
+		allStudentSchedules = [
+			...allStudentSchedules,
+			...(await Promise.all(
+				scrapableSchedulePromiseArray.map(
+					async (scrapableSchedulePromise): Promise<any[] | undefined> => {
+						/** scrape one-by-one */
+
+						try {
+							if (scrapableSchedulePromise.succeeded) {
+								return undefined;
+							}
+
+							const scrapedSchedule = await scrapableSchedulePromise.startedPromise;
+
+							scrapableSchedulePromise.succeeded = true;
+
+							return scrapedSchedule;
+						} catch (err) {
+							++howManyFailedThisLoop;
+
+							/** re-start the promise (retry) */
+							scrapableSchedulePromise.startedPromise = startPromise(
+								scrapableSchedulePromise.studentData
+							);
+
+							scrapableSchedulePromise.succeeded = false;
+
+							return undefined;
+						}
+					}
+				)
+			)),
+		];
+
+		++howManyTimesWeRetried;
+	} while (howManyFailedThisLoop > 0 && howManyTimesWeRetried < maxRetryCount);
+
+	/** remove `undefined` items */
+	allStudentSchedules = allStudentSchedules.filter((schedule) => !!schedule);
 
 	return allStudentSchedules;
 
