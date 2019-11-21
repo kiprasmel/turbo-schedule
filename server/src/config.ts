cf247f747fd72ed78b43538c41250240c33c93ff
Kipras Melnikovas <kipras@kipras.org>
2019-11-21 14:40:56 +0200

    Revert "refactor(server): Move `scrapedDataDirPath` into `generated/`"
    This reverts commit 59a0bba0e01758984d8e28a1a61e3fa69204f3bf.

--- server/src/config.ts
+++ server/src/config.ts
@@ -10,7 +10,7 @@ import { join } from "path";
 
 export const generatedDirPath: string = join(__dirname, "..", "generated"); /** dir for all generated stuff */
 export const openAPIFilePath = join(generatedDirPath, "openAPI.json");
-export const scrapedDataDirPath: string = join(generatedDirPath, "saved-content");
+export const scrapedDataDirPath: string = join(__dirname, "..", "saved-content");
 export const latestScrapedDataDirPath: string = join(scrapedDataDirPath, "latest");
 export const studentDataArrayFilePath: string = join(latestScrapedDataDirPath, "students-data-array.json");
 export const getStudentFilePath = (studentName: string): string =>
