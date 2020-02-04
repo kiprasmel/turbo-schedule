1309346fe05417cc9946d0990366da1b33df677b
Kipras Melnikovas <kipras@kipras.org>
2020-02-04 22:27:40 +0200

    refactor(scraper): Rewrite `getStudentsListHtml`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- scraper/src/util/getStudentsListHTML.ts
+++ scraper/src/util/getStudentsListHTML.ts
@@ -1,72 +1,44 @@
 import fs, { pathExists } from "fs-extra";
 
+import { IScraperConfig } from "../config";
 import { getHtml } from "./getHtml";
-import { fileIsAlreadySaved, getSavedFile, saveFile } from "./htmlSaving";
-import { getYYYYMMDD } from "@turbo-schedule/common";
-
-/** TODO config */
-const baseHtmlSaveFolder: string = "saved-content";
-
-/** necessary */
-const today: string = getYYYYMMDD();
-
-/** relative to file or from where the script was ran */
-// const savingPath: string = "../" + baseHtmlSaveFolder + "/" + today;
-const savingPath: string = baseHtmlSaveFolder + "/" + today;
-
-const savingFilename: string = "students-list.html";
-
-const savingPathAndFile: string = savingPath + "/" + savingFilename;
-
-/** the thing that's actually used by the utility functions */
 
 export const getStudentsListHtml = async (baseUrl: string): Promise<string> => {
 	try {
 		console.log("\n==> getStudentsListHtml:");
 
-		let todaysHtml: string;
-
-		if (fileIsAlreadySaved(savingPathAndFile)) {
-			console.log(" -> html IS saved, taking it from saved file");
-			todaysHtml = getSavedFile(savingPathAndFile);
-		} else {
-			console.log(" -> html IS NOT saved, getting it from url");
-			todaysHtml = await getHtml(baseUrl);
-			saveFile(todaysHtml, savingPath, savingFilename);
-		}
+		const todaysHtml: string = await getHtml(baseUrl);
 
 		return todaysHtml;
 	} catch (err) {
-		console.error(err);
-		return Promise.reject(new Error(err));
+		const e: Error = new Error(err);
+
+		console.error(e);
+		return Promise.reject(e);
 	}
 };
 
 /** TODO - only remake the symlink if the "latest" directory changed */
-export const updateLatestDir = async (): Promise<void> => {
-	/**
-	 *  TODO WARN
-	 *
-	 * Do this ONLY AFTER the script is finished,
-	 * meaning that ALL the required files are already available!!!
-	 */
-	// await fs.promises.symlink(baseHtmlSaveFolder + "/" + "latest", savingPath);
-	const symlinkFrom = baseHtmlSaveFolder + "/" + "latest";
-	const symlinkTo = today;
-
+export const updateLatestDir = async ({
+	latestScrapedDataSymlinkPath: symlinkFrom,
+	latestScrapedDataDirPath: symlinkTo,
+}: IScraperConfig): Promise<void> => {
 	try {
 		if (pathExists(symlinkFrom)) {
 			await fs.unlink(symlinkFrom);
 			console.log("\nUnlinked old symlink");
 		}
 	} catch (err) {
-		console.warn("\nFailed to unlink symlink, probably didn't exist yet", err);
+		/**
+		 * ignore - the symlink did not exist previously
+		 */
 	}
 
 	try {
+		console.log(`\nCreating symlink from ${symlinkFrom} to ${symlinkTo}`);
 		await fs.symlink(symlinkTo, symlinkFrom);
-		console.log("\nCreated new symlink");
 	} catch (err) {
-		console.error("\nFailed to symlink - this is bad!", err);
+		console.error("\nFailed creating symlink");
+		throw new Error(err);
 	}
 };
