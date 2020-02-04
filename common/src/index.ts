6bfce0401cf92dec6af6f94dee89a2afce877755
Kipras Melnikovas <kipras@kipras.org>
2020-02-04 18:38:57 +0200

    chore(common): Update exports
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,6 +1,9 @@
-export { getYYYYMMDD } from "./util/getYYYYMMDD";
-export { delay } from "./util/delay";
+export * from "./util/getYYYYMMDD";
+export * from "./util/delay";
+export * from "./util/applyMixins";
+export * from "./util/toJson";
+export * from "./util/writeToSingleFile";
 
-export { IStudent, Student } from "./model/Student";
-export { ILesson, Lesson } from "./model/Lesson";
-export { IEmail, Email } from "./model/Email";
+export * from "./model/Student";
+export * from "./model/Lesson";
+export * from "./model/Email";
