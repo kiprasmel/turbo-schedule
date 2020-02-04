fc9a1f06ff45fb3b37404c81f34947e508a68716
Kipras Melnikovas <kipras@kipras.org>
2020-02-04 21:50:08 +0200

    feat(scraper): Revamp `config` - single source of truth
    Finally!
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- scraper/src/config.ts
+++ scraper/src/config.ts
@@ -1,12 +1,75 @@
 // config.ts
 /**
+ * the exported values are the default ones.
+ * TODO should there even be the default exports?
+ * We'll use the dynamic ones anyway, right?
+ *
+ * You should create a config for yourself
+ * using the `createScraperConfig` function.
+ *
  * @note
  * paths are relative to THIS file (config.ts),
  * because we use `__dirname`
  * to keep everything consistant
  */
 
-// import { join } from "path";
+/* eslint-disable @typescript-eslint/interface-name-prefix */
 
-export const studentsPageURI: string = "http://kpg.lt/Tvarkarastis/Index.htm";
-export const baseScheduleURI: string = "http://kpg.lt/Tvarkarastis";
+import { join } from "path";
+import { getYYYYMMDD } from "@turbo-schedule/common";
+
+const defaultOutDir: string = join(__dirname, "scraped-content");
+
+export interface IScraperConfig {
+	/**  */
+	outDir: string;
+	/** dynamically generated based on `outDir`: */
+	latestScrapedDataSymlinkPath: string;
+	latestScrapedDataDirPath: string;
+	studentsFilePath: string;
+	uniqueLessonsFilePath: string;
+	studentsDirPath: string;
+	getStudentFilePath: (studentName: string) => string;
+}
+
+export const createScraperConfig = (outDir: string = defaultOutDir): IScraperConfig => {
+	/** BEGIN porting from server */
+
+	/**
+	 * TODO rename to `latestDatabasePath`
+	 */
+	const latestScrapedDataSymlinkPath: string = join(outDir, "latest");
+
+	/**
+	 * TODO FIXME - this is ONLY for symlinking - use `latestScrapedDataSymlinkPath` instead!
+	 *
+	 * TODO rename to `latestScrapedDataDirPathForSymlinking`
+	 */
+	const latestScrapedDataDirPath: string = join(outDir, getYYYYMMDD());
+
+	/**
+	 * TODO - the scraper needs to use the join with `latestScrapedDataDirPath`,
+	 * AND the consumer needs to use the join with `latestScrapedDataSymlinkPath`
+	 */
+	const studentsFilePath: string = join(latestScrapedDataDirPath, "students.json");
+	const uniqueLessonsFilePath: string = join(latestScrapedDataDirPath, "lessons.json");
+	const studentsDirPath: string = join(latestScrapedDataDirPath, "students");
+	const getStudentFilePath = (studentName: string): string => join(studentsDirPath, studentName + ".json");
+	/** END porting from server */
+
+	return {
+		/** */
+		outDir,
+		/** dynamically generated based on `outDir`: */
+		latestScrapedDataSymlinkPath,
+		latestScrapedDataDirPath,
+		studentsFilePath,
+		uniqueLessonsFilePath,
+		studentsDirPath,
+		getStudentFilePath,
+	};
+};
+
+// export const { latestScrapedDataDirPath, studentsFilePath, getStudentFilePath } = createScraperConfig(
+// 	defaultOutDir
+// );
