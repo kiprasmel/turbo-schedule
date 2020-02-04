4714083f85250d76d7af563530b8e500b39da124
Kipras Melnikovas <kipras@kipras.org>
2020-02-04 21:57:57 +0200

    BREAKING CHANGE(scraper): Rewrite `scraper` - use the latest features 🚀
    This is not a BREAKING CHANGE per se
    (it kinda is, but almost all dependents are internal,
    & we've made sure that external ones update to them successfully too).
    
    We're also marking this as a BREAKING CHANGE
    because it indicates a huge step for this project.
    
    Leggo! 🚀
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -1,57 +1,93 @@
-import { studentsPageURI, baseScheduleURI } from "./config";
-import { Student } from "./model/Student";
+import path from "path";
 
-import { getStudentsListHtml, updateLatestDir } from "./util/getStudentsListHTML";
-import { getStudentsDataArray } from "./util/scrapeStudents";
-import { getAllStudentScheduleHtmlInParallel } from "./getAllStudentScheduleHtmlInParallel";
+import { Student, StudentFromList, StudentWithNonUniqueLessons, writeToSingleFile } from "@turbo-schedule/common";
 
-import { createUniqueLessonsArray } from "./createUniqueLessonsArray";
-import { findLessonsForStudent } from "./findLessonsForStudent";
-import { IOptions } from "./options";
+import { IScraperConfig } from "./config";
+import { updateLatestDir } from "./util/getStudentsListHtml";
+import { getStudentList } from "./util/scrapeStudents";
+import { getAllStudentsFromListInParallel } from "./getAllStudentsFromListInParallel";
 
-import { move } from "fs-extra";
+import { populateStudentsWithUniqueLessons } from "./populateStudentsWithUniqueLessons";
+// import { populateStudentsWithFriends } from "./populateStudentsWithFriends";
 
-export const scrape = async (options: IOptions = { savePath: "saved-content" }): Promise<void> => {
-	/** TEMP - typescript fix - default params don't help -_- */
-	if (!options.savePath) {
-		options.savePath = "saved-content";
-	}
-
+export const scrape = async (config: IScraperConfig): Promise<void> => {
 	try {
 		console.log("\n==> scraper\n");
-		const studentsListHtml: string = await getStudentsListHtml(studentsPageURI);
+		console.table(config);
 
-		let studentsDataArray: Array<Student> = await getStudentsDataArray();
+		/**
+		 * TODO use functional programming and chain everything with `.map` yooo
+		 * umm actually, it's `.then`,
+		 * since we sometimes need the whole array instead of a single value,
+		 * iterated N times.
+		 */
 
-		/** BEGIN testing - add limit for quick runs */
-		// studentsDataArray = studentsDataArray.splice(0, 10);
-		/** END testing */
+		/**
+		 * TODO stop this pretty meh `memoize` logic
+		 * & only save the final students
+		 * at the end of the chain.
+		 */
 
-		/** TODO - returns wrongly! */
-		// const allStudentSchedulesArray__BAD_RETURN_FIXME: Array<any> = await getAllStudentScheduleHtmlInParallel(
-		// 	studentsDataArray
-		// );
-		await getAllStudentScheduleHtmlInParallel(studentsDataArray);
+		// const finalStudents: Student[] =
+		await getStudentList()
+			.then(
+				async (studentList: StudentFromList[]) => await writeToSingleFile(studentList, config.studentsFilePath)
+			)
+			// .then((studentList) => studentList.splice(0, 10)) /** uncomment this to quickly test a limited about of students */
+			.then((studentList: StudentFromList[]) => getAllStudentsFromListInParallel(studentList))
+			.then((studentsWithNonUniqueLessons: StudentWithNonUniqueLessons[]) =>
+				// // memoizeSync(config.uniqueLessonsFilePath, () =>
+				// // 	extractUniqueLessons(studentsWithNonUniqueLessons)
+				// extractUniqueLessons(studentsWithNonUniqueLessons).then((uniqueLessons: Lesson[]) =>
+				// 	populateStudentsWithUniqueLessons(studentsWithNonUniqueLessons, uniqueLessons)
+				// )
+				populateStudentsWithUniqueLessons(
+					studentsWithNonUniqueLessons,
+					config.uniqueLessonsFilePath
+					// extractUniqueLessons(studentsWithNonUniqueLessons)
+				)
+			)
+			/** BEGIN SOON */
+			// .then((students) => populateStudentsWithFriends(students))
+			// .then((studentsWithFriends) => {
+			// 	fs.writeFileSync(
+			// 		path.join(config.latestScrapedDataDirPath, "temp-students-with-friends.json"),
+			// 		prettier.format(
+			// 			JSON.stringify(
+			// 				studentsWithFriends
+			// 					.flatMap((student) => student.friends)
+			// 					.sort(
+			// 						(left: Friend, right: Friend) =>
+			// 							right.totalEncounters - left.totalEncounters ||
+			// 							left.text.localeCompare(right.text)
+			// 					)
+			// 			),
+			// 			{
+			// 				parser: "json",
+			// 			}
+			// 		),
+			// 		{ encoding: "utf-8" }
+			// 	);
 
-		// const uniqueLessonsArray: Array<any> = await createUniqueLessonsArray(allStudentSchedulesArray);
-		const uniqueLessonsArray: Array<any> = await createUniqueLessonsArray();
+			// 	return studentsWithFriends;
+			// })
+			/** END SOON */
+			/**
+			 * if you're adding features - add them here --
+			 * before the outcome is finally writen to the files.
+			 */
+			.then(
+				async (finalStudents: Student[]) =>
+					await Promise.all(
+						finalStudents.map(async (student) => {
+							writeToSingleFile(student, path.join(config.studentsDirPath, student.text + ".json"));
+							return student;
+						})
+					)
+				// await Student.writeManyToIndividualFiles(finalStudents, config.studentsDirPath)
+			);
 
-		studentsDataArray.forEach((student) => {
-			findLessonsForStudent(student.text, uniqueLessonsArray);
-		});
-
-		/** TODO fixme! */
-		updateLatestDir();
-
-		try {
-			/** TODO - this is NOT good lmao */
-			if (options.savePath !== "saved-content") {
-				await move("saved-content", options.savePath, { overwrite: true });
-				console.log("Moved content");
-			}
-		} catch (err) {
-			console.log("Failed moving, probly already exists");
-		}
+		await updateLatestDir(config);
 
 		console.log("\n -> scraper finished \n\n");
 		return;
