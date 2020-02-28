a8209881a25752a3c2de2306d6d52380869758b6
Kipras Melnikovas <kipras@kipras.org>
2020-02-28 23:14:34 +0200

    feat(db): Export the `defaultDbState`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- database/src/database.ts
+++ database/src/database.ts
@@ -8,14 +8,15 @@ export interface DbSchema {
 	lessons: Lesson[];
 }
 
+export const defaultDbState: DbSchema = { students: [], lessons: [] };
+
 export type Db = low.LowdbAsync<DbSchema>;
 
 const initDb = async (): Promise<Db> => {
 	const adapter: AdapterAsync<DbSchema> = new FileAsync<DbSchema>("database.json");
 	const db: Db = await low(adapter);
 
-	const dbDefaults: DbSchema = { students: [], lessons: [] };
-	db.defaults(dbDefaults);
+	db.defaults(defaultDbState);
 
 	return db;
 };
