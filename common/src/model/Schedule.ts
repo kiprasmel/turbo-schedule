af888e3ef4f928e7018391160c97961db406d69d
Kipras Melnikovas <kipras@kipras.org>
2020-04-13 00:20:52 +0300

    feat(common): Create `Schedule` model
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ common/src/model/Schedule.ts
@@ -0,0 +1,5 @@
+/**
+ * same for everyone - students, classes, teachers etc.
+ */
+export const frontPageScheduleURI: string = `http://kpg.lt/Tvarkarastis/Index.htm`;
+export const getSpecificScheduleURI = (href: string): string => `http://kpg.lt/Tvarkarastis/${href}`;
