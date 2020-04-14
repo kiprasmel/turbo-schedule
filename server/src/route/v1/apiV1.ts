3c734887609673b638218b2399d63b3fd478dbe0
Kipras Melnikovas <kipras@kipras.org>
2020-04-15 01:09:52 +0300

    feat(server): Create API endpoint for `schedule-item` 🚀
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/src/route/v1/apiV1.ts
+++ server/src/route/v1/apiV1.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { scheduleItemRouter } from "./schedule-item";
 import { classRouter } from "./class";
 import { studentRouter } from "./student";
 import { emailRouter } from "./email";
@@ -7,6 +8,7 @@ import { openAPIDocsJSONHandler, openAPIDocsHTMLHandler } from "./openAPIDocs";
 
 const router: Router = Router();
 
+router.use("/schedule-item", scheduleItemRouter); /** combines `class` & `student` */
 router.use("/class", classRouter);
 router.use("/student", studentRouter);
 router.use("/email", emailRouter);
