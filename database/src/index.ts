9a4f8692413e84b0ddf207a524f614cbbaddea8a
Kipras Melnikovas <kipras@kipras.org>
2020-04-22 01:41:05 +0300

    feat(db): Create a 100x simplier successor of `setNewDbState` - `setDbState` 🔥🔥🔥
    This is soooo much better 😭
    🚀 Inspired by & provides a fix for https://github.com/sarpik/turbo-schedule/issues/55
    🚀 Also fixes https://github.com/sarpik/turbo-schedule/issues/54
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- database/src/index.ts
+++ database/src/index.ts
@@ -1,5 +1,9 @@
 export * from "./config";
 export * from "./initDb";
+export * from "./setDbState";
+
+/** @deprecated */
 export * from "./setNewDbState";
+
 export * from "./fakeDb";
 export * from "./injectDb";
