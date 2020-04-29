10c8947b94e6f12ee6f6f37623adab43cf27c53a
Kipras Melnikovas <kipras@kipras.org>
2020-04-29 04:38:00 +0300

    fix(server): Default `/api/` to `/api/v1` (the latest API router)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/src/route/apiRouter.ts
+++ server/src/route/apiRouter.ts
@@ -5,4 +5,7 @@ const router: Router = Router();
 
 router.use("/v1", apiRouterV1);
 
+/** default to the latest API (MUST be the last route) */
+router.use("/", apiRouterV1);
+
 export { router as apiRouter };
