e16662fcb9a320bd21f7ee67c8ae481d2f944982
Kipras Melnikovas <kipras@kipras.org>
2020-05-28 01:17:27 +0300

    feat(client): Allow using `Header` & `Footer` conditionally
    choose yourself!
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- client/src/components/landing/Landing.tsx
+++ client/src/components/landing/Landing.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import Header from "../header/Header";
 import StudentList from "../studentList/StudentList";
+import Footer from "../footer/Footer";
 
-const Landing = () => <StudentList />;
+const Landing = () => (
+	<>
+		<Header />
+		<StudentList />
+		<Footer />
+	</>
+);
 
 export default Landing;
