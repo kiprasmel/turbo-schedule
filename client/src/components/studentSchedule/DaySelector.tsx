a51a2550a8ac6a4d6a7057c53101ef2aa8599bd7
Kipras Melnikovas <kipras@kipras.org>
2020-07-15 01:30:46 +0300

    fix(client): Improve DaySelector's styles & css
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- client/src/components/studentSchedule/DaySelector.tsx
+++ client/src/components/studentSchedule/DaySelector.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { css } from "emotion";
 import { scheduleDaysArray, ScheduleDay } from "../../utils/selectSchedule";
 
 export interface DaySelectorProps {
@@ -7,13 +8,35 @@ export interface DaySelectorProps {
 }
 
 const DaySelector: FC<DaySelectorProps> = ({ selectedDay, handleClick }) => (
-	<div style={{ width: "100%", position: "sticky", top: "0", background: "#fff" }}>
+	<div
+		className={css`
+			width: 100%;
+			position: sticky;
+			top: 0;
+			background: #fff;
+			z-index: 1;
+
+			& > * {
+				border-left: none !important;
+			}
+
+			& > *:last-child {
+				border-right: none !important;
+			}
+		`}
+	>
 		{scheduleDaysArray.map((dayIndex) => (
 			<button
+				type="button"
 				key={dayIndex}
 				onClick={(e) => handleClick(e, dayIndex)}
-				className={`btn${dayIndex === selectedDay ? " selected" : ""}`}
-				style={{ display: "inline-block", width: `${100 / scheduleDaysArray.length}%`, minHeight: "30px" }}
+				className={
+					css`
+						display: inline-block;
+						width: ${100 / scheduleDaysArray.length}%;
+						min-height: 30px;
+					` + ` btn${dayIndex === selectedDay ? " selected" : ""}`
+				}
 			>
 				<span>{dayIndex === "*" ? dayIndex : dayIndex + 1}</span>
 			</button>
