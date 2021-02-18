3bf22b5cdeba7fbf6a6a5ba36f2d06ed7cb9fd09
Kipras Melnikovas <kipras@kipras.org>
2021-02-18 06:10:32 +0200

    feat(database): create fake data if none available yet (ideal for new clones) 🚀
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- database/src/initDb.ts
+++ database/src/initDb.ts
@@ -3,11 +3,19 @@ import low, { AdapterAsync } from "lowdb";
 import FileAsync from "lowdb/adapters/FileAsync";
 
 import { databaseFile, Db, DbSchema, defaultDbState } from "./config";
+// eslint-disable-next-line import/no-cycle
+import { createFakeData } from "./script/module/createFakeData";
 
 const debug = require("debug")("turbo-schedule:database:initDb");
 
 export const initDb = async (storageFile: string = databaseFile): Promise<Db> => {
 	debug("Initializing database. `storageFile` = ", storageFile);
+
+	if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
+		if (!fs.pathExists(storageFile)) {
+			createFakeData();
+		}
+	}
 
 	await fs.ensureFile(storageFile);
 
