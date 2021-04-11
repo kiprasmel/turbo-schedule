8e82e0a66cb7e5e0d5c113077acca670e7af68f6
Kipras Melnikovas <kipras@kipras.org>
2021-04-11 16:45:31 +0300

    feat(db): create `copy-prod-data` script
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ database/copy-prod-data.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+REMOTE="${1:-prod}"
+
+ssh "$REMOTE" <<"EOF"
+docker cp turbo-schedule:/usr/src/app/database/data/latest.json /tmp/latest.json
+EOF
+
+DIR="$(dirname "$0")"
+
+rm "$DIR"/data/latest.json
+scp -r "$REMOTE":/tmp/latest.json "$DIR"/data/
