8121842042986e40ffe708ab2d35fcbd2c0b9509
Kipras Melnikovas <kipras@kipras.org>
2021-04-13 21:28:34 +0300

    fix(client): use LRUCache as single source of truth instead of an intermediary
    And use the localStorage only for backing up (as originally intended)
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- client/src/hooks/useLRUCache.ts
+++ client/src/hooks/useLRUCache.ts
@@ -14,23 +14,44 @@ export function createUsePersistedLRUCache<T = unknown>(
 
 		const [_persistedCache, _setPersistedCache] = useLocalStorage<T[]>(key, []);
 
-		useEffect(() => {
-			cache.current.addMany(_persistedCache);
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, []);
-
-		const add = useCallback(
-			(val: T) => {
-				cache.current.add(val);
-
-				_setPersistedCache(
-					newestOrOldest === "newest" ? cache.current.getAllNewToOld() : cache.current.getAllOldToNew()
-				);
-			},
+		const _updatePersistedCache = useCallback(
+			() =>
+				/**
+				 * here, internally, we should not pay attention to `newestOrOldest`
+				 * and always use the `OldToNew` approach, since it's idempotent
+				 *
+				 * otherwise, if we used `NewToOld`, we'd be reversing the order each time,
+				 * and you could refresh the page and see the order reversed each time
+				 * from the previous time
+				 */
+				_setPersistedCache(cache.current.getAllOldToNew()),
 			[_setPersistedCache]
 		);
 
-		return [_persistedCache, add];
+		/**
+		 * restore from the _persistedCache on load.
+		 * That's the whole reason we use the `_persistedCache` in the first place
+		 */
+		useEffect(() => {
+			cache.current.addMany(_persistedCache);
+			_updatePersistedCache();
+
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, []);
+
+		/**
+		 * wrap the `add` functionality to make sure the internal persisted cache
+		 * is always up to date.
+		 */
+		const add = useCallback(
+			(val: T) => {
+				cache.current.add(val);
+				_updatePersistedCache();
+			},
+			[_updatePersistedCache]
+		);
+
+		return [newestOrOldest === "newest" ? cache.current.getAllNewToOld() : cache.current.getAllOldToNew(), add];
 	};
 }
 
