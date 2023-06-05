2146392da8119845a5195637e0eea18e3730c6bc
Kipras Melnikovas <kipras@kipras.org>
2023-06-05 16:35:14 +0300

    move out student schedule machine into separate file

--- client/src/components/studentSchedule/StudentSchedule.tsx
+++ client/src/components/studentSchedule/StudentSchedule.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { css } from "emotion";
-import { assign, createMachine } from "xstate";
-import { useMachine } from "@xstate/react";
 
-import { Lesson, Student, ParticipantLabel, ArchiveLostFound, Participant } from "@turbo-schedule/common";
+import { Lesson, Student, ParticipantLabel } from "@turbo-schedule/common";
 
 import "./StudentSchedule.scss";
 
@@ -15,212 +13,15 @@ import StudentListModal from "./StudentListModal";
 import Loading from "../../common/Loading";
 import BackBtn from "../../common/BackBtn";
 
-import { fetchParticipantCore } from "../../hooks/useFetchers";
 import DaySelector from "./DaySelector";
 import { ScheduleDay, getTodaysScheduleDay } from "../../utils/selectSchedule";
 import { useTranslation } from "../../i18n/useTranslation";
 import { SchedulePageDesktop } from "./SchedulePageDesktop";
 import { LessonsList } from "./LessonsList";
-import { useQueryFor } from "../../hooks/useQueryFor";
+import { useStudentScheduleMachine } from "./student-schedule-machine";
 
 export interface IStudentScheduleProps {
 	match: any /** TODO */;
-}
-
-type MachineContext = {
-	scheduleByDays: Lesson[][];
-	snapshots?: string[]
-	snapshot?: string;
-}
-
-const defaultContext: MachineContext = {
-	scheduleByDays: [[]]
-};
-
-type MachineEvent = {
-	type: "FETCH_PARTICIPANT"
-} | {
-	type: "FETCH_SUCCESS",
-	scheduleByDays: Lesson[][]
-	snapshot?: string;
-} | {
-	type: "FETCH_FAILURE"
-} | {
-	type: "SEARCH_ARCHIVE_SUCCESS",
-	snapshots: string[];
-} | {
-	type: "SEARCH_ARCHIVE_FAILURE"
-} | {
-	type: "SELECT_ARCHIVE_SNAPSHOT",
-	snapshot: string
-} /* | {
-	type: "FETCH_ARCHIVE_SNAPSHOT_SUCCESS";
-	snapshot: string
-	scheduleByDays: Lesson[][]
-} | {
-	type: "FETCH_ARCHIVE_SNAPSHOT_FAILURE";
-	snapshot: string
-} */
-
-const studentScheduleMachine = createMachine<MachineContext, MachineEvent>({
-	id: "student-schedule",
-	initial: "idle",
-	context: defaultContext,
-	states: {
-		idle: {
-			"on": {
-				FETCH_PARTICIPANT: "fetch-participant",
-				SELECT_ARCHIVE_SNAPSHOT: {
-					target: "fetch-from-archive-snapshot",
-					actions: assign({
-						snapshot: (_, event) => event.snapshot,
-					})
-				}
-			}
-		},
-		"fetch-participant": {
-			entry: "fetchParticipant",
-			on: {
-				FETCH_SUCCESS: {
-					target: "loading-success",
-					actions: assign({
-						scheduleByDays: (_, e) => e.scheduleByDays,
-						snapshot: (c, e) => e.snapshot || c.snapshot
-					})
-				},
-				FETCH_FAILURE: "loading-failure"
-			}
-		},
-		"loading-success": {
-			on: {
-				FETCH_PARTICIPANT: "fetch-participant",
-				SELECT_ARCHIVE_SNAPSHOT: {
-					target: "fetch-from-archive-snapshot",
-					actions: assign({
-						snapshot: (_, event) => event.snapshot,
-					})
-				}
-			}
-		},
-		"loading-failure": {
-			entry: [
-				assign({ scheduleByDays: [[]] }),
-				"searchIfParticipantExistsInArchive"
-			],
-			on: {
-				SEARCH_ARCHIVE_SUCCESS: {
-					target: "search-archive-success",
-					actions: assign({
-						snapshots: (_, event) => event.snapshots
-					})
-				},
-				SEARCH_ARCHIVE_FAILURE: "search-archive-failure",
-			}
-		},
-		"search-archive-success": {
-			on: {
-				SELECT_ARCHIVE_SNAPSHOT: {
-					target: "fetch-from-archive-snapshot",
-					actions: assign({
-						snapshot: (_, event) => event.snapshot,
-					})
-				}
-			}
-		},
-		"search-archive-failure": {
-			on: {}
-		},
-		"fetch-from-archive-snapshot": {
-			entry: "fetchParticipant",
-			on: {
-				FETCH_SUCCESS: {
-					target: "loading-success",
-					actions: assign({
-						scheduleByDays: (_, e) => e.scheduleByDays,
-						snapshot: (c, e) => e.snapshot || c.snapshot,
-					})
-				},
-				FETCH_FAILURE: "loading-failure",
-			}
-		},
-	}
-});
-
-type UseStudentScheduleMachineOpts = {
-	studentName: string;
-	setParticipantType: React.Dispatch<React.SetStateAction<ParticipantLabel | null>>;
-}
-function useStudentScheduleMachine({ studentName, setParticipantType }: UseStudentScheduleMachineOpts) {
-	const [stateM, sendM] = useMachine(studentScheduleMachine, {
-		actions: {
-			fetchParticipant: (ctx): Promise<void> => ctx.snapshot ? fetchParticipant(studentName, ctx.snapshot) : fetchParticipant(studentName),
-			searchIfParticipantExistsInArchive: (): Promise<void> => searchIfParticipantExistsInArchive(),
-		}
-	});
-
-	const [snapshotParam] = useQueryFor("snapshot", {
-		encode: (x) => x,
-		decode: (x) => x,
-		dependencies: [stateM.context.snapshot],
-		valueOverrideOnceChanges: stateM.context.snapshot
-	});
-	console.log({snapshotParam});
-
-	useEffect(() => {
-		if (snapshotParam && snapshotParam !== stateM.context.snapshot) {
-			sendM({ type: "SELECT_ARCHIVE_SNAPSHOT", snapshot: snapshotParam });
-		} else {
-			sendM({ type: "FETCH_PARTICIPANT" });
-		}
-	}, [sendM, snapshotParam, stateM.context.snapshot]);
-
-	async function searchIfParticipantExistsInArchive(): Promise<void> {
-		const res = await fetch(`/api/v1/archive/lost-found?participantName=${encodeURIComponent(studentName)}`);
-
-		if (res.ok) {
-			const { found, snapshots }: ArchiveLost = await res.json();
-
-			if (found) {
-				sendM({ type: "SEARCH_ARCHIVE_SUCCESS", snapshots });
-			} else {
-				sendM({ type: "SEARCH_ARCHIVE_FAILURE" });
-			}
-		} else {
-			sendM({ type: "SEARCH_ARCHIVE_FAILURE" });
-		}
-	};
-
-	function onFetchParticipantResponse({ lessons, labels }: Participant, snapshot?: string): void {
-		setParticipantType(labels[0]);
-
-		if (!lessons?.length) {
-			sendM({ type: "FETCH_FAILURE" });
-			return;
-		}
-
-		const tempScheduleByDays: Array<Array<Lesson>> = [];
-
-		lessons.forEach((lesson) => {
-			/** make sure there's always an array inside an array */
-			if (!tempScheduleByDays[lesson.dayIndex]?.length) {
-				tempScheduleByDays[lesson.dayIndex] = [];
-			}
-
-			tempScheduleByDays[lesson.dayIndex].push(lesson);
-		});
-
-		sendM({ type: "FETCH_SUCCESS", scheduleByDays: tempScheduleByDays, snapshot });
-	};
-
-	function fetchParticipant(participant: string, snapshot?: string): Promise<void> {
-		console.log("fetching participant", participant);
-		return fetch(fetchParticipantCore[0](participant, snapshot))
-			.then((res) =>  res.json())
-			.then(data => onFetchParticipantResponse(data.participant, snapshot))
-			.catch(() => void sendM({ type: "FETCH_FAILURE" }));
-	}
-
-	return [stateM, sendM] as const;
 }
 
 const StudentSchedule = ({ match }: IStudentScheduleProps) => {
