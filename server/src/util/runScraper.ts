1c10818f763455924091d401d6e125ca37add8b4
Kipras Melnikovas <kipras@kipras.org>
2023-06-17 04:55:15 +0300

    fix: write ssh private key with 600 permissions to avoid error

--- server/src/util/runScraper.ts
+++ server/src/util/runScraper.ts
@@ -70,7 +70,9 @@ async function commitDatabaseDataIntoArchiveIfChanged(previousScrapeInfo: Scrape
 	const sshdir: string = path.join(homedir, ".ssh")
 	const sshFilepath: string = path.join(sshdir, "turbo-schedule-bot.rsa")
 	await mkdirAsync(sshdir, { recursive: true });
-	await writeAsync(sshFilepath, process.env.ARCHIVE_DEPLOY_KEY);
+	await writeAsync(sshFilepath, process.env.ARCHIVE_DEPLOY_KEY, {
+		mode: "600"
+	});
 
 	const newDbFilepath: string = getDatabaseFilepath(scrapeInfo.timeStartISO);
 	const dbDirPath: string = path.dirname(newDbFilepath);
