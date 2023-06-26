18167387e6dac5c862098866b92cf191d27c9f17
Kipras Melnikovas <kipras@kipras.org>
2023-06-26 20:15:06 +0300

    display pretty snapshot, also show if snapshot not found in archive but others found

--- database/src/index.ts
+++ database/src/index.ts
@@ -6,6 +6,7 @@ export * from "./get-db-snapshot-files";
 export * from "./read-raw-db";
 export * from "./commit-database-data-into-archive-if-changed";
 export * from "./get-meaningful-snapshots";
+export * from "./iso2file";
 
 export * from "./archive/snapshots-by-year";
 export * from "./archive/participants-in-snapshots";
